9a927f25b4c3252cd87759fc62307989c701d53b
aLVaRoZz01 <alvaro.rosado.g@gmail.com>
2024-02-27 22:49:06 +0100

    Added database and functionality to create users

--- App.js
+++ App.js
@@ -1,10 +1,75 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Platform, TouchableOpacity } from 'react-native';
+import * as SQLite from 'expo-sqlite';
+import { useState, useEffect } from 'react';
+
 
 export default function App() {
+  const [db, setDb] = useState(SQLite.openDatabase('perico.db'));
+  const [isLoading, setIsLoading] = useState(true);
+  const [names, setNames] = useState([]);
+  const [currentName, setCurrentName] = useState(undefined);
+
+
+  useEffect(() => {
+    db.transaction(tx => {
+      tx.executeSql('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)')
+    });
+
+    db.transaction(tx => {
+      tx.executeSql('SELECT * FROM users', null,
+        (txObj, resultSet) => setNames(resultSet.rows._array),
+        (txObj, error) => console.log(error)
+      );
+    });
+
+
+    db.transaction(tx => {
+      //tx.executeSql('DELETE FROM users')
+    });
+
+
+    setIsLoading(false);
+  }, [db]);
+
+  if (isLoading) {
+    return (
+      <View style={styles.container}>
+        <Text>Loading names...</Text>
+      </View>
+    );
+  }
+
+  const addName = () => {
+    db.transaction(tx => {
+      tx.executeSql('INSERT INTO users (name) values (?)', [currentName],
+        (txObj, resultSet) => {
+          let existingNames = [...names];
+          existingNames.push({ id: resultSet.insertId, name: currentName});
+          setNames(existingNames);
+          setCurrentName(undefined);
+        },
+        (txObj, error) => console.log(error)
+      );
+    });
+  }
+
+
+  const showNames = () => {
+    return names.map((name, index) => {
+      return (
+        <TouchableOpacity key={index} style={styles.button}>
+          <Text style={styles.buttonText}>{name.name}</Text>
+        </TouchableOpacity>
+      );
+    });
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.container}>
+      <TextInput value={currentName} placeholder='Nombre' onChangeText={setCurrentName} style={styles.input} />
+      <Button title="Añadir Nombre" onPress={addName} />
+      {showNames()}
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +81,26 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 5,
+    padding: 8,
+    marginTop: 8,
+    marginBottom: 8,
+    width: '80%',
+  },
+  button: {
+    backgroundColor: '#4CAF50',
+    padding: 10,
+    margin: 5,
+    borderRadius: 5,
+    width: '80%',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
   },
 });
